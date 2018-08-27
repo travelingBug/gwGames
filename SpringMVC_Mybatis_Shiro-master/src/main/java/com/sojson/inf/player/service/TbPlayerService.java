da599da898a06f08a91e9f83d8a180f0b1a9a16c
lx514014 <lx514014@163.com>
2018-08-27 23:18:47 +0800

    参赛人员列表

--- SpringMVC_Mybatis_Shiro-master/src/main/java/com/sojson/inf/player/service/TbPlayerService.java
+++ SpringMVC_Mybatis_Shiro-master/src/main/java/com/sojson/inf/player/service/TbPlayerService.java
@@ -1,15 +1,7 @@
 package com.sojson.inf.player.service;
 
 import com.sojson.common.ResultMessage;
-import com.sojson.common.model.UUser;
-import com.sojson.core.mybatis.page.Pagination;
 import com.sojson.inf.player.model.dto.TbPlayerDto;
-import com.sojson.permission.bo.URoleBo;
-import com.sojson.permission.bo.UserRoleAllocationBo;
-import org.springframework.ui.ModelMap;
-
-import java.util.List;
-import java.util.Map;
 
 public interface TbPlayerService {
 
