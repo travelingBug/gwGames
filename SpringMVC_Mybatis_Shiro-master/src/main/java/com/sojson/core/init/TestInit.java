1b0ee9854d5cc3bee24d59fe46e5bb5770747394
hyhyhy108 <hyhyhy108@163.com>
2018-09-01 23:35:07 +0800

    系统初始化运行

---
+++ SpringMVC_Mybatis_Shiro-master/src/main/java/com/sojson/core/init/TestInit.java
@@ -0,0 +1,19 @@
+package com.sojson.core.init;/**
+ * @Author: hy
+ * @Description:
+ * @Date:${Time} ${Date}
+ **/
+
+/**
+ * @ClassName:TestInit
+ * @Description:TODO
+ * @Author:yamol
+ * @Date:2018-9-1 23:29
+ * @VERSION: 1.0
+ */
+public class TestInit {
+    public void init(){
+
+        System.out.println(1111);
+    }
+}
