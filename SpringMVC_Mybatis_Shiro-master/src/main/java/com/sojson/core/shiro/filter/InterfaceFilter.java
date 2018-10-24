a34926c9240b899ca1ae8a82e4307632fe8c47b5
hanwenjiang <236632716@qq.com>
2018-10-24 20:17:29 +0800

    Merge branch 'master' of C:\Users\Administrator\IdeaProjects\gwGames with conflicts.

--- SpringMVC_Mybatis_Shiro-master/src/main/java/com/sojson/core/shiro/filter/InterfaceFilter.java
+++ SpringMVC_Mybatis_Shiro-master/src/main/java/com/sojson/core/shiro/filter/InterfaceFilter.java
@@ -13,7 +13,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class InterfaceFilter extends AccessControlFilter {
 	@Override
